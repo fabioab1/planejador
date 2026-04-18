4fb0d68ca834e08fbd258ad7caf9fb291aecca43
fabioab1 <fabiobonanatodeabreu990@gmail.com>
2026-04-18 20:36:31 -0300

    feat: implementa as funcionalidades de CRUD para as tarefas no localStorage e a sua manipulação

---
+++ script.js
@@ -0,0 +1,126 @@
+let tarefas = [];
+
+carregarTarefas();
+renderizarTarefas();
+
+function salvarTarefas() {
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
+
+
+function carregarTarefas() {
+    const dados = localStorage.getItem("tarefas");
+    tarefas = dados ? JSON.parse(dados) : [];
+}
+
+function criarTarefa(descricao, prioridade, data) {
+    const nova = {
+        id: Date.now().toString(),
+        descricao,
+        prioridade,
+        coluna: "entrada",
+        data: data || null,
+        concluida: false,
+        criadaEm: Date.now()
+    }
+
+    tarefas.push(nova);
+    salvarTarefas();
+    renderizarTarefas();
+}
+
+document.querySelector("button").addEventListener("click", () => {
+    const desc = document.querySelector("input[type=text]").value;
+    const prio = document.querySelector("select").value;
+    const data = document.querySelector("input[type=date]").value;
+
+    criarTarefa(desc, prio, data);
+});
+
+function renderizarTarefas() {
+    document.querySelectorAll(".column").forEach(col => col.innerHTML = "");
+
+    tarefas
+        .filter(t => !t.concluida)
+        .forEach(tarefa => {
+            const card = criarCard(tarefa);
+            document.getElementById(tarefa.coluna).appendChild(card);
+        });
+}
+
+function criarCard(tarefa) {
+    const div = document.createElement("div");
+    div.className = `card card-${tarefa.prioridade}`;
+    div.draggable = true;
+    div.dataset.id = tarefa.id;
+
+    div.innerHTML = `
+        <strong>${tarefa.descricao}</strong>
+        ${tarefa.data ? `<div>📅 ${tarefa.data}</div>` : ""}
+
+        <div class="card-actions">
+            <button onclick="editar('${tarefa.id}')">✏️</button>
+            <button onclick="concluir('${tarefa.id}')">✔️</button>
+        </div>
+    `;
+
+    adicionarEventosDrag(div);
+    return div;
+}
+
+function editar(id) {
+    const tarefa = tarefas.find(t => t.id === id);
+    const novaDesc = prompt("Editar tarefa:", tarefa.descricao);
+
+    if (novaDesc) {
+        tarefa.descricao = novaDesc;
+        salvarTarefas();
+        renderizarTarefas();
+    }
+}
+
+function concluir(id) {
+    const tarefa = tarefas.find(t => t.id === id);
+    tarefa.concluida = true;
+
+    salvarTarefas();
+    renderizarTarefas();
+}
+
+function deletar(id) {
+    tarefas = tarefas.filter(t => t.id !== id);
+    salvarTarefas();
+    renderizarTarefas();
+}
+
+let tarefaArrastada = null;
+
+function adicionarEventosDrag(card) {
+    card.addEventListener("dragstart", () => {
+        tarefaArrastada = card.dataset.id;
+    });
+}
+
+document.querySelectorAll(".column").forEach(coluna => {
+
+    coluna.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    });
+
+    coluna.addEventListener("drop", () => {
+        moverTarefa(tarefaArrastada, coluna.id);
+    });
+
+});
+
+function moverTarefa(id, novaColuna) {
+    const tarefa = tarefas.find(t => t.id === id);
+    tarefa.coluna = novaColuna;
+
+    salvarTarefas();
+    renderizarTarefas();
+}
+
+function filtroHoje (data) {
+    
+}
